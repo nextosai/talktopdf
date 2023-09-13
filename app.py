2c5e631d39c2326e7e102e45be793fe370263bf9
nextosai <guile@nextos.ai>
2023-09-12 21:19:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,10 @@ def handle_userinput(user_question):
 
 
 def main():
-    load_dotenv()
+    load_dotenv(override=True)
+    
+    OPENAI_API_KEY='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'
+    
     st.set_page_config(page_title="Digital Minds 2023",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
